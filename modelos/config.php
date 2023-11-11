90032ccd8a3fe589f56dbfd16fb9651021869c6b
Sandra <pepperander@hotmail.com>
2023-11-11 10:32:21 +0100

    Comienzo de proyecto

---
+++ modelos/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', '');
+define('MYSQL_DB', 'blog');
+define('MYSQL_HOST', 'localhost');
+
+?>
---
+++ modelos/usuario.php
@@ -0,0 +1,75 @@
+<?php 
+
+    class Usuario {
+        private $id;
+        private $sid;
+        private $email;
+        private $password;
+        private $nombre;
+        private $telefono;
+        private $poblacion;
+
+    // Métodos para acceder a los atributos
+        public function getId(){
+            return $this->id;
+        }
+        public function setId($id): self{
+            $this->id = $id;
+            return $this;
+        }
+
+
+        public function getSid(){
+            return $this->sid;
+        }
+        public function setSid($sid): self{
+            $this->sid = $sid;
+            return $this;
+        }
+
+
+        public function getEmail(){
+            return $this->email;
+        }
+        public function setEmail($email): self{
+            $this->email = $email;
+            return $this;
+        }
+
+
+        public function getPassword(){
+            return $this->password;
+        }
+        public function setPassword($password): self{
+            $this->password = $password;
+            return $this;
+        }
+
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+        public function setNombre($nombre): self{
+            $this->nombre = $nombre;
+            return $this;
+        }
+
+
+        public function getTelefono(){
+            return $this->telefono;
+        }
+        public function setTelefono($telefono): self{
+            $this->telefono = $telefono;
+            return $this;
+        }
+
+        public function getPoblacion(){
+            return $this->poblacion;
+        }
+        public function setPoblacion($poblacion): self{
+            $this->poblacion = $poblacion;
+            return $this;
+        }
+    }
+
+?>
